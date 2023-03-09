51662106e2fc2ea81abcec6378664d8ed321b090
J-Rod97 <90074658+J-Rod97@users.noreply.github.com>
2023-03-09 14:55:58 -0500

    PICKER

---
+++ Profile/Profile/Picker Selection.swift
@@ -0,0 +1,35 @@
+//
+//  Picker Selection.swift
+//  Profile
+//
+//  Created by Jonathan Rodriguez on 3/9/23.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    @State private var choices = ["green", "blue", "red"]
+    @State private var selectedChoice = "green"
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(choices, id: \.self) { choice in
+                    Button {
+                        selectedChoice = choice
+                    } label: {
+                        Text(choice)
+                            .foregroundColor(selectedChoice == choice ? .green : .orange)
+                    }
+                    .buttonStyle(.bordered)
+                }
+            }
+        }
+    }
+}
+                    
+struct PickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerView()
+    }
+}
