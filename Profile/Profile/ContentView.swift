d1000ffe0584f33c571c2529506ea428292a7a7d
Jonathan Rodriguez <rodri1jr@cmich.edu>
2023-03-08 23:57:10 -0500

    SAVE ME THE HEADACHE

---
+++ Profile/Profile/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Profile
+//
+//  Created by Jonathan Rodriguez on 3/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Image(systemName:"bell.fill")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Profile/Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Jonathan Rodriguez on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
